b365ec4723ae012f8e227d891d00cda44cd53cf9
YiWei Zheng <yiweizhe@usc.edu>
2012-11-28 16:11:29 -0800

    changing to nonblocking assignment

--- generateopen.py
+++ generateopen.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
 
 for num in range(0,400):
-    print ('openx[%d]= {0,0,0,0,0,0,0,0};' % (num))
-    print ('openy[%d]= {0,0,0,0,0,0,0,0};' % (num))
+    print ('openx[%d] <= {0,0,0,0,0,0,0,0};' % (num))
+    print ('openy[%d] <= {0,0,0,0,0,0,0,0};' % (num))
