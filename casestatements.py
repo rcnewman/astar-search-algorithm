2d8a7b826a915f42ac29f72b4568e351d27aeb38
YiWei Zheng <yiweizhe@usc.edu>
2012-12-01 14:44:51 -0800

    script

--- casestatements.py
+++ casestatements.py
@@ -2,4 +2,4 @@ print ('--==CHECK IF NEIGHBOR IS BETTER==--')
 print ('case(currentx) begin')
 for num in range(0,40):
     print('8\'%s:' % bin(num)[1:])
-    print('neighbor_distance_from_start = distanceFromStart&d[currentx]+ ((currentx == tempneighborx[neighborcounter] || currenty == tempneighbory[neighborcounter]) ? 10 : 19);' % (num)); 
+    print('neighbor_distance_from_start = distanceFromStart%d[currentx]+ ((currentx == tempneighborx[neighborcounter] || currenty == tempneighbory[neighborcounter]) ? 10 : 19);' % (num)); 
