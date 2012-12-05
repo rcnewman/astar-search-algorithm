734bb1c2e25c566147ff3227fa2fae11f8f148b7
YiWei Zheng <yiweizhe@usc.edu>
2012-12-04 18:49:28 -0800

    py

--- generateopen.py
+++ generateopen.py
@@ -5,15 +5,12 @@ for num in range(0,40):
 for num in range(0,400):
     print ('openx[%d] <= 8\'b0;' % (num))
     print ('openy[%d] <= 8\'b0;' % (num))
-
 for num in range(0,40):
     print ('   reg [7:0]   previousNode%d [39:0];' % (num))
-
 for num in range(0,40):
     print ('   reg [7:0]   distanceFromStart%d [39:0];' % (num))
-
 for num in range(0,40):
-    print ('			 distanceFromStart%d[temp1] = 8\'b11111111;' % (num))
+    print ('			 distanceFromStart%d[temp1] <= 8\'b11111111;' % (num))
 for num in range(0,400):
     print('closex[%d] <= 8\'b0;' % (num))
     print('closey[%d] <= 8\'b0;' % (num))
