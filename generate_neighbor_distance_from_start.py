0eb5bef3037d41129afc120421f05b770f3705e1
Yiwei Zheng <yiweizhe@usc.edu>
2012-12-05 21:24:44 -0800

    more logic

--- generate_neighbor_distance_from_start.py
+++ generate_neighbor_distance_from_start.py
@@ -1,1 +1,11 @@
-write it
+print('case(currentx)')
+for num in range(0,40):
+    print ('8\'%s:' % bin(num)[1:])
+    print ('begin')
+    print ('case(currenty')
+    for num2 in range(0,40):
+	    print ('8\'%s:\nbegin\nneighbor_distance_from_start <= distanceFromStart%d[%d]*1000 + (currentx == tempneighborx || currenty == tempneighbory) ? 1000 : 1414;\nend' % (bin(num2)[1:],num,num2))
+    print ('endcase')
+    print ('end')
+	
+	//
