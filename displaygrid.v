4b82859b0ae49143f3b45868df1bea2cbc4e65f2
Ross Newman <rcnewman@usc.edu>
2012-12-05 21:02:24 -0800

    reconstruct done

--- displaygrid.v
+++ displaygrid.v
@@ -4,9 +4,20 @@ begin
 	for(integer j = 0; j < 40; j = j + 1)
 	begin
 		if(map[i][j] == 1)
+			begin
 			$write("X");
+			end
 		else if (finished_map[i][j] == 1)
+			begin
 			$write("P");
+			end
+		else if (currentx == j)
+			begin
+			if(currenty == i)
+				begin
+				$write("A");
+				end
+			end
 		else
 			$write("O");
 	end
