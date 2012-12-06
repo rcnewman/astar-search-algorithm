ecad4903f8a0c5bf252306aa74b9fd6b2a798884
Yiwei Zheng <yiweizhe@usc.edu>
2012-12-05 19:48:14 -0800

    mode/sscript

--- reconstruct.py
+++ reconstruct.py
@@ -1,3 +1,27 @@
 for num in range(0,40):
     print ("finished_path_x[%d] <= 8'b11111111;" % (num))
     print ("finished_path_y[%d] <= 8'b11111111;" % (num))
+for num in range(0,40):
+    print('8\'%s:' % bin(num)[1:])
+    print("begin\nfor (i = 0; i < 16; i = i +1) begin")
+    print('$write("%d",distanceFromStart$d[i]);' % (num))
+    print('end')
+    print('$display(" ");')
+    print('end')
+
+	
+	
+for num in range(0,40):
+    print('8\'%s:' % bin(num)[1:])
+    print('begin')
+    print('finished_path_x[recon_counter] <= previousNodeX%d[current_recon_y];' % (num))
+    print('current_recon_x <= previousNodeX%d[current_recon_y];' % (num))
+    print('end')
+
+for num in range(0,40):
+    print('8\'%s:' % bin(num)[1:])
+    print('begin')
+    print('finished_path_y[recon_counter] <= previousNodeY%d[current_recon_x];' % (num))
+    print('current_recon_y <= previousNodeY%d[current_recon_x];' % (num))
+    print('end')
+	
