c509b904bb42ef93c84b457c12bf380f8941d9e0
Yiwei Zheng <yiweizhe@usc.edu>
2012-12-05 21:08:29 -0800

    more code

--- neighborIsBetter.py
+++ neighborIsBetter.py
@@ -3,6 +3,6 @@ for num in range(0,40):
     print ('begin')
     print ('case(tempneighborx[neighborcounter])')
     for num2 in range(0,40):
-	    print ('8\'%s:\nbegin\npreviousNode%d[%d] <= currentx;\nend' % (bin(num)[1:], num, num2))
+	    print ('8\'%s:\nbegin\npreviousNodeX%d[%d] <= currentx;\npreviousNodeY%d[%d] <= currenty;\ndistanceFromStart%d[%d] <= neighbor_distance_from_start;\nend' % (bin(num)[1:], num, num2, num, num2, num, num2))
     print ('endcase')
     print ('end')
