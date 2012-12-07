9a4083fcc6d3ce1349227843c31200019a0d87cb
Yiwei Zheng <yiweizhe@usc.edu>
2012-12-06 17:57:26 -0800

    motteke

--- reconstruct_standalone.v
+++ reconstruct_standalone.v
@@ -1,12 +1,18 @@
 RECONSTRUCT:
 		 begin
 			//$display("STATE: RECONSTRUCT");
-			recon_counter <= 0;
+			recon_counter <= 10'b0;
 			current_recon_x <= 8'b0010_0111;
 			current_recon_y <= 8'b0010_0111;
 			$display("X:%d Y:%d",current_recon_x,current_recon_y);
 	$display("prevX:%d prevY:%d", previousNodeX39[current_recon_y],previousNodeY39[current_recon_x]);
 	
+	
+	$display("39 39: %d,%d",previousNodeX39[39],previousNodeY39[39]);
+	$display("39 38: %d,%d",previousNodeX39[38],previousNodeY39[38]);
+	$display("38 39: %d,%d",previousNodeX38[39],previousNodeY38[39]);
+	$display("38 38: %d,%d",previousNodeX38[38],previousNodeY38[38]);
+	$display("38 37: %d,%d",previousNodeX38[37],previousNodeY38[37]);
 			state <= FIND_PREVIOUS;
 		 end //STATE RECONSTRUCT
 		 
@@ -430,8 +436,10 @@ end
 		if(current_recon_x == 0 && current_recon_y == 0)
 			state <= DONE;
 		else
+		begin
 			recon_counter <= recon_counter + 1;
 			state <= FIND_PREVIOUS;
+			end
 	end
 	DONE:
 	begin
